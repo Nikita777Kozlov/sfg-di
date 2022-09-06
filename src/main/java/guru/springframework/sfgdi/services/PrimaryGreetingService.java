74799f90dbece7ac54aac2fdb96f76e1860a1659
ITD\Kazlou_MS <mister.nikita2000@bk.ru>
2022-09-06 13:29:29 +0300

    Added configuration package and class for example of using @Configuration and @Bean annotations

--- src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PrimaryGreetingService.java
@@ -1,10 +1,6 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Service;
 
-@Service
-@Primary
 public class PrimaryGreetingService implements GreetingService{
 
     @Override
